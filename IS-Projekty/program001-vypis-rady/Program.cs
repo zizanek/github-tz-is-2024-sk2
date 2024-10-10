581d3612a8de34f85bd5bedf4984dcb6c6a6c0a8
Tomáš Žižka <tomas.zizka@tul.cz>
2024-10-10 13:31:51 +0200

    #1 zakladni program s moznosti opakovani

---
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Specialized;
+
+class Program {
+
+    static void Main() {
+
+        // chci, aby se program opakoval po stisku klávesy "a"
+        string again = "a";
+        while(again == "a") {
+            //Console.Clear();
+            Console.WriteLine("**************************");
+            Console.WriteLine("******* Výpis řady *******");
+            Console.WriteLine("**************************");
+            Console.WriteLine("****** Tomáš Žižka *******");
+            Console.WriteLine("**************************\n\n");
+            Console.WriteLine();
+
+            // Vstup od uživatele - TO-DO
+
+            // Logika pro výpis řady - TO-DO
+
+
+            // Opakování programu
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
+    }   
+
+
+}
+
+
