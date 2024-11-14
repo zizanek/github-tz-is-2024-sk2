bbc5762c47aaae722c3dfb4425d606ac6ac39cf3
Tomáš Žižka <tomas.zizka@tul.cz>
2024-11-14 12:44:08 +0100

    #7 bubble sort - prázdný projekt

---
+++ IS-Projekty/program007-bubble-sort/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Security.Authentication.ExtendedProtection;
+
+string again = "a";
+        while(again == "a") {
+            //Console.Clear();
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("******* Generátor pseudonáhodných čísel *******");
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("**************** Tomáš Žižka ******************");
+            Console.WriteLine("***********************************************\n\n");
+            Console.WriteLine();
+
+            
+            Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
+            int n;
+            while(!int.TryParse(Console.ReadLine(), out n)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte počet čísel znovu (celé číslo): ");
+            }
+
+            Console.Write("Zadejte dolní mez (celé číslo): ");
+            int dm;
+            while(!int.TryParse(Console.ReadLine(), out dm)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu dolní mez (celé číslo): ");
+            }
+
+            Console.Write("Zadejte horní mez (celé číslo): ");
+            int hm;
+            while(!int.TryParse(Console.ReadLine(), out hm)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu horní mez (celé číslo): ");
+            }
+
+            Console.WriteLine("\n\n====================");
+            Console.WriteLine("Uživatelský vstup: ");
+            Console.WriteLine("Počet čísel: {0}; dolní mez: {1}; horní mez: {2}", n, dm, hm);
+            Console.WriteLine("====================\n\n");
+
+            // deklarace pole
+            int[] myArray = new int[n];
+
+            // příprava pro generování náhodných čísel
+            Random randomNumber = new Random();
+
+            Console.WriteLine("Náhodná čísla: ");
+
+            for(int i=0; i<n; i++) {
+                myArray[i] = randomNumber.Next(dm, hm+1);
+                Console.Write("{0}; ", myArray[i]);
+            }
+            
+            // Opakování programu
+            Console.WriteLine("\n\nPro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
